87a4d69c5db2199b3640bce76e0b16612034e60a
Jennifer Brabec <jenniferbrabec@gmail.com>
2017-02-12 22:26:23 +0000

    helper_functions in progress

--- helper_functions.py
+++ helper_functions.py
@@ -1,4 +1,4 @@
-from model import db
+from model import db, Topic, Meeting, Rating
 
 
 def query_meetings():
@@ -12,13 +12,28 @@ def query_meetings():
 
     return results
 
+
 def query_recurring_meetings():
-    """Helper function used by query database for all meetings"""
+    """Helper function used by query database for recurring meetings"""
 
-    recur_mtg = db.session.query(Meeting).filter_by(recurring_id=1)
+    dicto = {}
+    # labels = []
+    # data = []
 
-    # perform db query and get all results
-    cursor = db.session.execute(sql_query)
-    results = cursor.fetchall()
+    meetings = Meeting.query.filter_by(recurring_id=1).all()
 
-    return results
+    # meetings = db.session.query(
+    #                             Meeting.meeting_title,
+    #                             Meeting.meeting_time,
+    #                             # Rating.score).all()
+    #                             Rating.score).filter_by(Meeting.recurring_id=1)
+# for mtg in meetings:
+#     print mtg.meeting_title
+#     for rating in mtg.rating:
+#         print rating.score
+
+    for mtg in meetings:
+        title = meeting_title
+        dicto[mtg.meeting_time] = dicto.get(mtg.rating.score)
+
+    return
