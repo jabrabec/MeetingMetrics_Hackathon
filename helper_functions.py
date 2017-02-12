eea347d4d47ea5813a90e17c2fd398a0a921554a
Jennifer Brabec <jenniferbrabec@gmail.com>
2017-02-12 21:49:53 +0000

    helper_functions progress

--- helper_functions.py
+++ helper_functions.py
@@ -11,3 +11,14 @@ def query_meetings():
     results = cursor.fetchall()
 
     return results
+
+def query_recurring_meetings():
+    """Helper function used by query database for all meetings"""
+
+    recur_mtg = db.session.query(Meeting).filter_by(recurring_id=1)
+
+    # perform db query and get all results
+    cursor = db.session.execute(sql_query)
+    results = cursor.fetchall()
+
+    return results
